eaaa5ebc5068133d15e4432dc64ca0354796cd73
Casper Ho <casperyaoz@gmail.com>
2025-02-06 21:34:26 +0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+  if (
+    !Number.isInteger(number1) ||
+    !Number.isInteger(number2) ||
+    number1 < 0 ||
+    number2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  const start = Math.min(number1, number2);
+  const end = Math.max(number1, number2);
+
+  sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
