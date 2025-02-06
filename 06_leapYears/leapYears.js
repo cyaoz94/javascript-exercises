93c18a505dfefc501c5ae19b065a6e24d908836a
Casper Ho <casperyaoz@gmail.com>
2025-02-06 21:45:52 +0800

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // if year is century but not divisble by 400
+  if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  }
 
+  // if year not divisible by 4
+  if (year % 4 !== 0) {
+    return false;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
