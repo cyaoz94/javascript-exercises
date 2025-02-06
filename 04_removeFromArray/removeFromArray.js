cd15ac09cc286fdb1b46f80ac127cb9eb673cb12
Casper Ho <casperyaoz@gmail.com>
2025-02-06 21:18:30 +0800

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...value) {
+  return array.filter((word) => !value.includes(word));
 };
 
 // Do not edit below this line
