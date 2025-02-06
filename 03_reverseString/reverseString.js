050e300cec4a41153b88c8239d3bc1076c8ef188
Casper Ho <casperyaoz@gmail.com>
2025-02-06 21:13:18 +0800

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let array = string.split("");
+  let reversedArray = array.reverse();
+  return reversedArray.join("");
 };
 
 // Do not edit below this line
