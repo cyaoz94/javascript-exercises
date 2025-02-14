1761a2dc56644b63f58f7b9bd2731f531b52dfa5
Casper Ho <casperyaoz@gmail.com>
2025-02-14 16:46:16 +0800

    Complete 08_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // remove comma and full stop
+  // remove spaces
+  string = string.replace(/[^\w\']|_/g, "").toLowerCase();
+  reversedString = string.split("").reverse().join("");
 
+  console.log(string, reversedString);
+  return string === reversedString;
 };
 
 // Do not edit below this line
