0cf5f68e7056df9c34e8d3acc2de2a5e338109e9
Casper Ho <casperyaoz@gmail.com>
2025-02-14 15:39:30 +0800

    Complete 06_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+// (32°F − 32) × 5/9 = 0°C
+const convertToCelsius = function (value) {
+  let celciusValue = ((value - 32) * 5) / 9;
+  return Math.round(celciusValue * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// (0°C × 9/5) + 32 = 32°F
+const convertToFahrenheit = function (value) {
+  let fahrenheitValue = (value * 9) / 5 + 32;
+  return Math.round(fahrenheitValue * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
