f43bf151cbb042ae71a2a7237b2c129bec30ea50
Casper Ho <casperyaoz@gmail.com>
2025-02-14 15:59:14 +0800

    Complete 07_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (value1, value2) {
+  return value1 + value2;
 };
 
-const subtract = function() {
-	
+const subtract = function (value1, value2) {
+  return value1 - value2;
 };
 
-const sum = function() {
-	
+const sum = function (values = []) {
+  if (values.length === 0) {
+    return 0;
+  }
+
+  return values.reduce((total, value) => total + value);
 };
 
-const multiply = function() {
+const multiply = function (values = []) {
+  if (values.length === 0) {
+    return 0;
+  }
 
+  return values.reduce((total, value) => total * value);
 };
 
-const power = function() {
-	
+const power = function (value, power) {
+  return Math.pow(value, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (value) {
+  if (value < 0) {
+    return "ERROR";
+  }
+
+  if (value === 0) {
+    return 1;
+  }
+
+  return value * factorial(value - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
