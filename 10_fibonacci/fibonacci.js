630d3d93cee0c7a208c18d83969f4d40ced13bda
Casper Ho <casperyaoz@gmail.com>
2025-02-14 17:09:50 +0800

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (typeof number === "string") {
+    number = Number(number);
+  }
 
+  if (number < 0) {
+    return "OOPS";
+  }
+
+  if (number <= 1) {
+    return number;
+  }
+
+  return fibonacci(number - 1) + fibonacci(number - 2);
+
+  // fibonacci(4) = fibonacci(3) + fibonacci(2) = 2 + 1 = 3
+  // fibonacci(3) = fibonacci(2) + fibonacci(1) = 1 + 1 = 2
+  // fibonacci(2) = fibonacci(1) + fibonacci(0) = 1 + 0 = 1
+  // fibonacci(1) =  1
+  // fibonacci(0) = 0
+  // fibonacci(2) = 1 + 0 = 1
 };
 
 // Do not edit below this line
